fb98044de2137aeb74d4e5999032c9120c0024db
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-10 16:25:50 -0700

    Created filter options component

---
+++ src/components/FilterOptions/FilterOptions.jsx
@@ -0,0 +1,12 @@
+import "./FilterOptions.css";
+
+const FilterOptions = () => {
+  return (
+    <div className="filterOptions">
+      <input type="text" placeholder="Search" />
+      <input type="text" />
+    </div>
+  );
+};
+
+export default FilterOptions;
