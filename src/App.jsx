f13a4bd3c5ed49bba20d549d8f6365be2476329a
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-10 23:03:32 -0700

    enabled toggling screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import FilterOptions from "./components/FilterOptions/FilterOptions";
 import Footer from "./components/Footer/Footer";
-import MoviesList from "./components/MoviesList/MoviesList";
 import Navbar from "./components/Navbar/Navbar";
+import SearchScreen from "./screens/SearchScreen/SearchScreen";
+import NowPlayingScreen from "./screens/NowPlayingScreen/NowPlayingScreen";
 
 const App = () => {
-  const [moviesList, setMoviesList] = useState([]);
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjdiNDBjODkyMGJjNzMyODk1MTU0M2IyM2MwMjg3YSIsInN1YiI6IjY2Njc2NDY0YTdmNzVmZDVhODFjMzAyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C6GUy0On998UxcO7SY7sKs6S06ECbxQJ_Vsav1vGusw",
-    },
-  };
-
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => setMoviesList(response.results))
-      .catch((err) => console.error(err));
-  });
+  const [isSearching, setIsSearching] = useState(true);
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar isSearching={isSearching} setIsSearching={setIsSearching} />
       <div className="content">
-        <FilterOptions />
-        <MoviesList moviesList={moviesList} />
+        {isSearching ? <SearchScreen /> : <NowPlayingScreen />}
       </div>
       <Footer />
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ isSearching, setIsSearching }) => {
   return (
     <div className="navbar">
       <h1>Flixster</h1>
+      {isSearching ? (
+        <div className="toggle-button" onClick={() => setIsSearching(false)}>
+          See Now Playing
+        </div>
+      ) : (
+        <div className="toggle-button" onClick={() => setIsSearching(true)}>
+          Search For Movies
+        </div>
+      )}
     </div>
   );
 };
