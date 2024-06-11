36368e18f8cfd805361e5948517b135e855a5bc6
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-10 17:28:45 -0700

    fetched movie data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import FilterOptions from "./components/FilterOptions/FilterOptions";
+import Footer from "./components/Footer/Footer";
+import MoviesList from "./components/MoviesList/MoviesList";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-  <div className="App">
-    
-  </div>
-}
+  const [moviesList, setMoviesList] = useState([]);
 
-export default App
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjdiNDBjODkyMGJjNzMyODk1MTU0M2IyM2MwMjg3YSIsInN1YiI6IjY2Njc2NDY0YTdmNzVmZDVhODFjMzAyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C6GUy0On998UxcO7SY7sKs6S06ECbxQJ_Vsav1vGusw",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setMoviesList(response.results))
+      .catch((err) => console.error(err));
+  });
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <FilterOptions />
+        <MoviesList movies={moviesList} />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
