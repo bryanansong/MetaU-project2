cd7c36ced60a0dfe5f8617ade52751be3cefc831
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-11 11:21:24 -0700

    Added sort dropdown menu

--- src/components/FilterOptions/FilterOptions.jsx
+++ src/components/FilterOptions/FilterOptions.jsx
@@ -1,10 +1,21 @@
 import "./FilterOptions.css";
 
-const FilterOptions = () => {
+const FilterOptions = ({ setSearchQuery, setSortType }) => {
   return (
     <div className="filterOptions">
-      <input type="text" placeholder="Search" />
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(event) => setSearchQuery(event.target.value)}
+      />
+      <select onChange={(e) => setSortType(e.target.value)}>
+        <option value="none">Select Sorting</option>
+        <option value="title_asc">Title | Ascending</option>
+        <option value="title_desc">Title | Reverse</option>
+        <option value="popularity">Popularity</option>
+        <option value="date">Release Date</option>
+        <option value="vote_average">Average</option>
+      </select>
     </div>
   );
 };
